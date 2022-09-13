97213a6e2ad41378d497a090a29aa62dbcffceee
Mohammed Zohil PK <44064613+zoypk@users.noreply.github.com>
2022-09-13 23:40:30 +0530

    Update sw.c

--- sw.c
+++ sw.c
@@ -10,28 +10,29 @@ noframes=rand()/200;
 i=1;
 j=1;
 noframes = noframes / 8;
-printf("\n Number of frames is %d",noframes);
+printf("\n number of frames is %d",noframes);
 while(noframes>0)
 {
-printf("\nSending frame %d",i);
+printf("\nsending frame %d",i);
 srand(x1++); //The srand() function sets the starting point for producing a series of pseudo-random integers
 x = rand()%10;
 if(x%2 == 0)
 {
 for (x2=1; x2<2; x2++)
 {
-printf("\nWaiting for %d seconds\n", x2);
-sleep(x2);
-printf("Missing Acknowledgement %d",i);
+         printf("waiting for %d seconds\n", x2);
+ sleep(x2);
+ printf("Missing Acknowledgement %d",i);
 }
-printf("\nSending frame %d",i);
+printf("\nsending frame %d",i);
 srand(x1++);
 x = rand()%10;
 }
-printf("\nAck received for frame %d",j);
+printf("\nack received for frame %d",j);
 noframes-=1;
 i++;
 j++;
 }
-printf("\n End of stop and wait protocol");
+printf("\n end of stop and wait protocol");
 }
+~        
